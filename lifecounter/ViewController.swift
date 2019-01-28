49c3ae058d17a8e31a04d7c331e8e4194c90b31c
Nick Hytrek <nshytrek@gmail.com>
2019-01-28 12:01:28 -0800

    added buttons and labels for player one

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Nicholas S. Hytrek on 1/28/19.
+//  Copyright © 2019 Nicholas S. Hytrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var playerOneLife: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func playerOnePlusOne(_ sender: UIButton) {
+    }
+    
+    @IBAction func playerOnePlusfive(_ sender: UIButton) {
+    }
+    
+    @IBAction func playerOneMinusOne(_ sender: UIButton) {
+    }
+    
+    @IBAction func playerOneMinusFive(_ sender: UIButton) {
+    }
+    
+}
+
