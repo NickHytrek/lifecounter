e93e87cf1cda24c7aebbd04e4355ecd48e2940a8
Nick Hytrek <nshytrek@gmail.com>
2019-01-30 13:17:28 -0800

    edited layout for lifecounter 2, added history page

---
+++ lifecounter/historyPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  historyPageViewController.swift
+//  lifecounter
+//
+//  Created by Nicholas S. Hytrek on 1/30/19.
+//  Copyright © 2019 Nicholas S. Hytrek. All rights reserved.
+//
+
+import UIKit
+
+class historyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
